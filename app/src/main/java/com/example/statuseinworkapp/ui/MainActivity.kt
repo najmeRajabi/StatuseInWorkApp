2530ae42e7ace839bb6f101caed2b60d7c0defe7
niha8515 <niha8515@yahoo.com>
2022-05-26 09:45:20 +0430

    add layout

--- app/src/main/java/com/example/statuseinworkapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/statuseinworkapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.statuseinworkapp
+package com.example.statuseinworkapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.statuseinworkapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
